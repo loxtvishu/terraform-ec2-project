ebcf7e13ff531042c61050984f131ae182aa5a9f
loxtvishu <2003vishusaini@gmail.com>
2026-04-15 09:19:42 +0000

    Terraform EC2 setup with S3 backend

--- Ec2_Creation.tf
+++ Ec2_Creation.tf
@@ -1,20 +1,27 @@
-# Configure AWS Provider
+terraform {
+  backend "s3" {
+    bucket = "loxtvishu-dev-terraform-ap-south-1-state"
+    key    = "ec2/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 provider "aws" {
-  region = "ap-south-1" # You can change it to your preferred region
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "pathnex" {
+resource "aws_instance" "loxtvishu_ec2_dev" {
   ami                    = "ami-0f559c3642608c138"
-  instance_type          = "m7i-flex.large" # You can change it according to your need
+  instance_type          = "t3.micro"
   count                  = 1
-  key_name               = "Pathnex-ec2-key" # change if key name is different in aws account
-  subnet_id              = "subnet-060e30fbb2ffaa858" # Change it with your subnet id
-  vpc_security_group_ids = ["sg-006c76c984d9e1310"] # Change it with your security group
+  key_name               = "DevOps-key"
+  subnet_id              = "subnet-03d562807e78675b1"
+  vpc_security_group_ids = ["sg-0a93e5aefec4dabef"]
+
   tags = {
-    Name = "pathnex-ec2"
+    Name = "loxtvishu-dev-pathnex-ec2-ap-south-1-01"
   }
 }
 
 output "ec2_public_ips" {
-  value = aws_instance.pathnex[*].public_ip
+  value = aws_instance.loxtvishu_ec2_dev[*].public_ip
 }
